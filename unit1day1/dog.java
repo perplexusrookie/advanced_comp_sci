d43956473a55ce5eb1cb31e6bf5ebbe5396532af
Lu <lschwarzman1@hwemail.com>
2026-08-25 13:30:18 -0700

    completed lesson

---
+++ unit1day1/dog.java
@@ -0,0 +1,12 @@
+public class dog {
+    public static void main(String[] args) {
+        System.out.println("woof");
+        System.out.println("bark bark");
+        System.out.println("This dog has a stick. \nIt's a good boy!");
+        // \n is escape character
+
+        System.out.println("Name:\tGrace");
+        System.out.print("Has Tail: yes");
+        System.out.print("\nColor: brown");
+    }
+}
---
+++ unit1day1/lesson.java
@@ -0,0 +1,6 @@
+public class lesson {
+    // method -> main method entry point for the java program
+    public static void main(String[] args){
+        System.out.println("hewwo world :3")
+    }
+}
